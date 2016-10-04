f0aac752298ba04f3fb348fe5c2c9a30ebd42444
voliy <volconfiy@gmail.com>
2016-10-05 02:28:30 +0300

    Add compile-time weaving sample

--- src/main/java/foo/AnotherAspect.java
+++ src/main/java/foo/AnotherAspect.java
@@ -5,9 +5,9 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
 @Aspect
-public class SampleAspect {
+public class AnotherAspect {
     @Before("execution(* foo.SampleService.printRandomValue(..))")
     public void logBefore(JoinPoint joinPoint) {
-        System.out.println("SampleAspect, joinPoint: " + joinPoint.getSignature().getName());
+        System.out.println("AnotherAspect, joinPoint: " + joinPoint.getSignature().getName());
     }
 }
